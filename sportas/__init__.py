4d7c3a6de625875210e4ce6a10c0806e756ae1fe
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-15 17:58:57 +0300

    Update __init__.py

--- sportas/__init__.py
+++ sportas/__init__.py
@@ -1,6 +1,24 @@
 import check50
 
+with open('U1.txt') as f:
+    lines = f.read().split()
+    
 @check50.check()
 def exists():
   """sportas.cpp egzistuoja."""
   check50.exists("sportas.cpp")
+  
+@check50.check(exists)
+def compiles():
+    """sportas.cpp kompiliuojasi be klaidų."""
+    check50.run("g++ sportas.cpp -lcrypt -lcs50 -lm -o aliejus").exit(0)
+   
+@check50.check(compiles)
+def exists_txt():
+    """U1.txt egzistuoja."""
+    check50.exists("U1.txt")
+   
+@check50.check(compiles)
+def exists_reztxt():
+    """U1rez.txt egzistuoja."""
+    check50.exists("U1rez.txt")
