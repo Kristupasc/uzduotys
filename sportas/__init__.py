6335568774fc985fdbc76a7ca391815e1c9f5f2d
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-15 18:35:08 +0300

    Update __init__.py

--- sportas/__init__.py
+++ sportas/__init__.py
@@ -34,10 +34,5 @@ def test0():
 
 def test1():
     """Teisingai irasyta i rezultatus"""
-    check50.run("> U1rez.txt").exit(0)
-    check50.run("./sportas").exit(0)
-    with open('U1rez.txt') as f1:
-        linesRez = f1.read().split()
-        if(len(linesRez) < 1):
-            raise check50.Failure("File U1rez.txt nepakanka duomenų")
+
         
