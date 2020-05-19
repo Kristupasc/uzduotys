86e2bc5483dd70706c8bd3a733bf991c57aa4fb3
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-19 12:55:13 +0300

    Update __init__.py

--- sportas/__init__.py
+++ sportas/__init__.py
@@ -46,15 +46,15 @@ def test1():
             else:
                 raise check50.Failure("Neteisingas numeris")
                 
-@check50.check(compiles)
+@check50.check(exists)
 def test2():
     """Gaunamas teisingas atsakymas pagal 1 pavyzdį"""
     check50.run("> U1rez.txt"_).exit(0)
     duomenys = open("U1.txt","w")
-    L = ["6 \n", "Petras A. Petraitis 213 15 20 00 \n", "Jurgis Jurgutis 221 16 12 12 \n", "Rima Joana 115 15 15 59 \n",
-         "Zigmas Nosis 256 16 23 9 \n", "Roma Liepa 111 15 15 15 \n", "Rytis Uosis Ainis 255 16 23 9 \n", "5 \n", 
-         "256 16 43 15 5 5 5 5 \n", "213 15 50 10 4 0 5 3 \n", "111 16 5 35 5 4 \n", "255 16 55 59 5 4 3 1 \n", "115 16 42 22 2 5 \n"]
-    duomenys.writelines(L)
+    #L = ["6 \n", "Petras A. Petraitis 213 15 20 00 \n", "Jurgis Jurgutis 221 16 12 12 \n", "Rima Joana 115 15 15 59 \n",
+         #"Zigmas Nosis 256 16 23 9 \n", "Roma Liepa 111 15 15 15 \n", "Rytis Uosis Ainis 255 16 23 9 \n", "5 \n", 
+         #"256 16 43 15 5 5 5 5 \n", "213 15 50 10 4 0 5 3 \n", "111 16 5 35 5 4 \n", "255 16 55 59 5 4 3 1 \n", "115 16 42 22 2 5 \n"]
+    duomenys.write("Hello")
     duomenys.close()
     file = open("U1.txt", "r+")
     raise check50.Failure(file.read())
