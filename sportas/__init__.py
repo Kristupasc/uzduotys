87cb2bf76ee40b47cd345485d97080d48034b3a6
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-19 19:52:06 +0300

    Update __init__.py

--- sportas/__init__.py
+++ sportas/__init__.py
@@ -42,8 +42,8 @@ def test1():
          "256 16 43 15 5 5 5 5 \n", "213 15 50 10 4 0 5 3 \n", "111 16 5 35 5 4 \n", "255 16 55 59 5 4 3 1 \n", "115 16 42 22 2 5 \n"]
     duomenys.writelines(L)
     duomenys.close()
-    check50.run("> U1rez.txt").exit(0)
-    check50.run("./sportas").exit(0)
+    #check50.run("> U1rez.txt").exit(0)
+    #check50.run("./sportas").exit(0)
     #file = open("U1rez.txt", "r+")
     #TODO
     #ats = [" "]
