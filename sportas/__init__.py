c204bb49a098a3a2ecdd74fa8086fdfd2db38b24
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-20 09:18:18 +0300

    Update __init__.py

--- sportas/__init__.py
+++ sportas/__init__.py
@@ -51,7 +51,7 @@ def test1():
     #check50.run("> U1rez.txt").exit(0)
     #check50.run("./aliejus").exit(0)
     with open('U1rez.txt') as f1:
-        linesRez = f1.read()
+        linesRez = f1.read().split().split()
     eilutes = 2 + int(lines[sk])
     if not linesRez:
         raise check50.Failure("U1rez.txt yra tusčias")
