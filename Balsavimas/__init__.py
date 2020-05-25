3b794e00406eb86b5a350cd184987cdcf42c8671
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-25 13:32:51 +0300

    Update __init__.py

--- Balsavimas/__init__.py
+++ Balsavimas/__init__.py
@@ -32,3 +32,38 @@ def test0():
         raise check50.Failure("U1.txt yra užrašytas neteisingai.")
     elif len(lines) > k:
         raise check50.Failure(k)
+
+@check50.check(compiles)
+def test1():
+    """Informacija faile U1rez.txt yra išvedama teisingai"""
+    check50.run("> U1rez.txt").exit(0)
+    check50.run("./sportas").exit(0)
+    with open("U1rez.txt") as m:
+        rez = m.read().split()
+    linesRez = len(open("U1rez.txt").readlines())
+    eilutes = 3
+    if not linesRez:
+        raise check50.Failure("U1rez.txt yra tusčias")
+    if linesRez < eilutes:
+        raise check50.Failure("U1rez.txt išvedė per mažai eilučių.")
+    if linesRez > eilutes:
+        raise check50.Failure("U1rez.txt išvedė per daug eilučių.")
+        
+@check50.check(compiles)
+def pvz1():
+    """Gaunamas teisingas atsakymas pagal 1 pavyzdį"""
+    check50.run("> U1.txt").exit(0)
+    duomenys = open("U1.txt","w")
+    L = ["6 \n", "15 10 22 \n", "15 40 13 \n", "23 26 26 \n", "110 30 58 \n", "33 33 32 \n", "0 56 0 \n", "2 1 3 \n"]
+    duomenys.writelines(L)
+    duomenys.close()
+    #check50.run("> U1rez.txt").exit(0)
+    #check50.run("./sportas").exit(0)
+    with open("U1rez.txt") as m:
+        rez = m.read().split()
+    ats = ["196", "195", "151", "6", "12", "6", "2"]
+    if ats == rez:
+        pass
+    else:
+        raise check50.Mismatch(ats, rez)
+      
