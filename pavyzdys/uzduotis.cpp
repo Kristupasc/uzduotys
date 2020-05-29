a81c264992d9cdb61e549e89588f18d46e82cc6f
Kristupasc <56727667+Kristupasc@users.noreply.github.com>
2020-05-29 16:32:25 +0300

    Create uzduotis.cpp

---
+++ pavyzdys/uzduotis.cpp
@@ -0,0 +1,12 @@
+//Tai yra c++ failas, į kūrį bus rašomas visas kodas.
+//Kad sutaupyti mokinio laiką, galima į šį failą įdėti šablono kodą:
+
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+
+int main()
+{
+  
+  return 0;
+}
